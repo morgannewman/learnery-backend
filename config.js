4d3a22d0ea460741904c877a765f7633b8556971
Morgan Newman <mpnewman95@gmail.com>
2018-11-26 15:15:11 -0800

    Minor fixes to deployment config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  POSTGRES_DB_URL: process.env.POSTGRES_DB_URL || 'postgresql://dev:@localhost/learnery',
+  POSTGRES_DB_URL: process.env.DATABASE_URL || 'postgresql://dev:@localhost/learnery',
   JWT_SECRET: process.env.JWT_SECRET || 'this_is_not_a_secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',
   PORT: process.env.PORT || 8080
