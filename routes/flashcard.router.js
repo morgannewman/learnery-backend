5a8ce0a98e454d1e13270e25db8a6dc7ef3ba7fd
Morgan Newman <mpnewman95@gmail.com>
2018-11-28 13:07:24 -0800

    Adds logic to return next flashcard

--- routes/flashcard.router.js
+++ routes/flashcard.router.js
@@ -2,39 +2,72 @@ const router = require('express').Router();
 const models = require('../models/index.js');
 
 router.get('/', (req, res, next) => {
-  console.log(req.user);
-  let { username } = req.user;
-  models.User.findOne({
-    where: {
-      username
-    }
-  }).then(user => {
-    console.log(user.dataValues.queue);
-    let firstItemPosition = user.dataValues.queue[0];
-    return models.Flashcard.findOne({
-      where: {
-        id: firstItemPosition
-      }
-    }).then(card => {
-      let flashC = {
-        question: card.dataValues.question,
-        answer: card.dataValues.answer
-      };
-      res.json(flashC);
-    });
-  });
+	let user;
+	models.User.findOne({
+		where: {
+			username: req.user.username
+		}
+	}).then(_user => {
+		user = _user;
+		console.log(user.dataValues.queue);
+		let nextId = user.dataValues.queue[0].id;
+		return models.Flashcard.findOne({
+			where: {
+				id: nextId
+			}
+		}).then(card => {
+			let flashC = {
+				question: card.dataValues.question,
+				answer: card.dataValues.answer
+			};
+			res.json(flashC);
+		});
+	});
 });
 
-// router.post('/', (req, res, next) => {
-//   let { answer } = req.body;
+function constructNewQueue(queue, M = null) {
+	const top = { ...queue[0] };
+	// Set M to be M*2 or the given M
+	if (!M) M = top.M *= 2;
+	top.M = M;
+	return [...queue.slice(1, M), top, ...queue.slice(M)];
+}
 
-//   if(answer === 'Yikes') {
-
-//   }
-
-//   if(answer === 'Got It!') {
-
-//   }
-// });
+router.post('/', async (req, res, next) => {
+	// Find user
+	const userModel = await models.User.findOne({
+		where: {
+			username: req.user.username
+		}
+	});
+	// Grab their queue
+	let { queue } = userModel.dataValues;
+	// Construct new queue based on their answer
+	const { confidence } = req.body;
+	switch (confidence) {
+		case '0':
+			queue = constructNewQueue(queue, 1);
+			break;
+		case '1':
+			queue = constructNewQueue(queue);
+			break;
+	}
+	// send user new item
+	// update DB with new queue
+	const [flashcard] = await Promise.all([
+		models.Flashcard.findOne({ where: { id: queue[0].id } }),
+		userModel.update(
+			{ queue },
+			{
+				where: {
+					username: req.user.username
+				},
+				fields: ['queue'],
+				returning: false
+			}
+		)
+	]);
+	return res.json(flashcard);
+});
 
 module.exports = router;
