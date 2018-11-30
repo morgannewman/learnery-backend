be9a2a54232aa6008ce071dbd3ffa9fd77a9541a
Morgan Newman <mpnewman95@gmail.com>
2018-11-30 09:07:28 -0800

    Updates seed data with stats

--- db/data.js
+++ db/data.js
@@ -1,18 +1,135 @@
 const cards = [
-  { question: 'Are u nice', answer: 'no' },
-  { question: 'Are u happy', answer: 'yes' },
-  { question: 'Are u ambivalent', answer: 'maybe' },
-  { question: 'Are u ?', answer: '?' },
-  { question: 'Are u nice', answer: 'no' },
-  { question: 'Are u happy', answer: 'yes' },
-  { question: 'Are u ambivalent', answer: 'maybe' },
-  { question: 'Are u ?', answer: '?' }
+  { question: '__PLACEHOLDER__', answer: '__PLACEHOLDER__' },
+  {
+    question: 'What is the difference between <script>, <script async> and <script defer>?',
+    answer:
+			'Normal scripts are blocking, so page render pauses until the script downloads and runs. Async scripts do not block rendering while downloading, but they do run as soon as they load. Defer scripts download and run after all HTML and CSS renders.'
+  },
+  {
+    question: 'When should you use a button vs. a link?',
+    answer:
+			'Buttons should be used for interactions within a page, or interactions that don\'t always cause you to leave a page (e.g. form submission). Links should be used for interactions that will always leave the current page.'
+  },
+  { question: 'How can you vertically center an element without using Flexbox?', answer: '__PLACEHOLDER__' },
+  {
+    question: 'How can you design a stack with a max() method that has O(1) lookup time?',
+    answer: '__PLACEHOLDER__'
+  },
+  {
+    question: 'Is it possible to prevent a form submission from reloading the page without JavaScript?',
+    answer:
+			'Yes. Although you cannot prevent a form from reloading the page after it has been submitted w/o JavaScript, you can have the form be disabled by default and simply enable it with JavaScript.'
+  },
+  {
+    question: 'What happens when you add a number to a string?',
+    answer: 'The number is coerced to a string, then they are added together.'
+  },
+  { question: '__PLACEHOLDER__', answer: '__PLACEHOLDER__' }
+];
+
+const seedDataQueue = [
+  {
+    M: 2,
+    id: 4,
+    stats: {
+      streak: 1,
+      lastSeen: '2018-11-27T12:56:57.676Z',
+      maxStreak: 1,
+      timesSeen: 3,
+      timesCorrect: 1,
+      timesIncorrect: 2
+    }
+  },
+  {
+    M: 4,
+    id: 2,
+    stats: {
+      streak: 1,
+      lastSeen: '2018-11-28T15:56:57.676Z',
+      maxStreak: 2,
+      timesSeen: 4,
+      timesCorrect: 3,
+      timesIncorrect: 1
+    }
+  },
+  {
+    M: 2,
+    id: 6,
+    stats: {
+      streak: 1,
+      lastSeen: '2018-11-25T15:52:57.676Z',
+      maxStreak: 1,
+      timesSeen: 3,
+      timesCorrect: 1,
+      timesIncorrect: 2
+    }
+  },
+  {
+    M: 4,
+    id: 8,
+    stats: {
+      streak: 2,
+      lastSeen: '2018-11-25T16:56:57.676Z',
+      maxStreak: 2,
+      timesSeen: 4,
+      timesCorrect: 2,
+      timesIncorrect: 2
+    }
+  },
+  {
+    M: 8,
+    id: 1,
+    stats: {
+      streak: 3,
+      lastSeen: '2018-11-25T15:58:57.676Z',
+      maxStreak: 3,
+      timesSeen: 4,
+      timesCorrect: 3,
+      timesIncorrect: 1
+    }
+  },
+  {
+    M: 8,
+    id: 3,
+    stats: {
+      streak: 3,
+      lastSeen: '2018-11-25T13:55:57.676Z',
+      maxStreak: 3,
+      timesSeen: 3,
+      timesCorrect: 3,
+      timesIncorrect: 0
+    }
+  },
+  {
+    M: 16,
+    id: 7,
+    stats: {
+      streak: 4,
+      lastSeen: '2018-11-25T14:56:57.676Z',
+      maxStreak: 4,
+      timesSeen: 4,
+      timesCorrect: 4,
+      timesIncorrect: 0
+    }
+  },
+  {
+    M: 32,
+    id: 5,
+    stats: {
+      streak: 5,
+      lastSeen: '2018-11-25T14:58:57.676Z',
+      maxStreak: 5,
+      timesSeen: 5,
+      timesCorrect: 5,
+      timesIncorrect: 0
+    }
+  }
 ];
 
 const users = [
-  { username: 'potato1', email: 'email1@test.com', password: 'password', queue: constructQueue() },
-  { username: 'tomato2', email: 'email2@test.com', password: 'password', queue: constructQueue() },
-  { username: 'brosato3', email: 'email3@test.com', password: 'password', queue: constructQueue() }
+  { username: 'potato1', email: 'email1@test.com', password: 'password', queue: seedDataQueue },
+  { username: 'tomato2', email: 'email2@test.com', password: 'password', queue: seedDataQueue },
+  { username: 'brosato3', email: 'email3@test.com', password: 'password', queue: seedDataQueue }
 ];
 
 function constructQueue() {
@@ -28,7 +145,8 @@ function constructQueue() {
         timesIncorrect: 0,
         streak: 0,
         maxStreak: 0,
-        lastSeen: null
+        lastSeen: null,
+        timesSeen: 0
       }
     });
   }
--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const jwtStrategy = require('./passport/jwt');
 // routers
 const authRouter = require('./routes/auth.router');
 const flashCardRouter = require('./routes/flashcard.router');
+const statsRouter = require('./routes/stats.router');
 // Create an Express application
 const app = express();
 
@@ -41,11 +42,11 @@ passport.use(jwtStrategy);
 
 // Mount routers
 app.use('/auth', authRouter);
-app.use(
-  '/api',
-  passport.authenticate('jwt', { session: false, failWithError: true })
-);
+// Protect all /api endpoints
+app.use('/api', passport.authenticate('jwt', { session: false, failWithError: true }));
 app.use('/api/flashcard', flashCardRouter);
+app.use('/api/stats', statsRouter);
+
 // Custom 404 Not Found route handler
 app.use((req, res, next) => {
   const err = new Error('Not Found');
@@ -65,14 +66,12 @@ app.use((err, req, res, next) => {
 });
 
 if (require.main === module) {
-  app
-    .listen(PORT, function() {
-      console.info(`Server listening on ${this.address().port}`);
-    })
-    .on('error', err => {
-      console.error(err);
-      console.log('Is the postgres service running? Try $ pg_ctl start');
-    });
+  app.listen(PORT, function() {
+    console.info(`Server listening on ${this.address().port}`);
+  }).on('error', err => {
+    console.error(err);
+    console.log('Is the postgres service running? Try $ pg_ctl start');
+  });
 }
 
 module.exports = app; // Export for testing
